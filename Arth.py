69fdd2e7d15ed803df9782466f17f68fc5267b8d
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-12-15 19:32:27 +0530

    Arthmetic_operator

---
+++ Arth.py
@@ -0,0 +1,20 @@
+num_1=2344
+num_2=23.45
+num_3=34+45j
+num_4=23
+print("num_1 : ",num_1)
+print("num_2 : ",num_2)
+print("num_3 : ",num_3)
+print("num_4 : ",num_4)
+sum=num_1+num_2
+print("Sum of num_1 & and num_2 : ",sum)
+sub=num_2-num_3
+print("Sub of  num_2 & num_3 : ",sub)
+mul=num_1*num_4
+print("Mul of num_1 & num_4 : ",mul)
+mod=num_4%num_1
+print("mod of num_4 & num_1 : ",mod)
+floor=num_4//num_2
+print("Floor of num_4 & num_2 : ",floor)
+exp=num_1**num_4
+print("exp of num_1 & num_4 : ",exp)
