415ca0379fe608c32f30f8eb53701d96755a7376
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-12-15 20:09:51 +0530

    operators

---
+++ Arthmetic/Assign.py
@@ -0,0 +1,16 @@
+num_1=2345
+print("value of num_1 is : ",num_1)
+num_1 += 20
+print("value of num_1 is : ",num_1)
+num_1 -= 20
+print("value of num_1 is : ",num_1)
+num_1 *= 20 
+print("value of num_1 is : "num_1)
+num_1 /= 20 
+print("value of num_1 is : "num_1)
+num_1 %= 20
+print("value of num_1 is : ",nun_1)
+num_1 //= 20 
+print("value of num_1 is : "num_1)
+num_1 **= 20 
+print("value of num_1 is : ",num_1)
---
+++ Assignment/Assign.py
@@ -0,0 +1,17 @@
+num_1=2345
+print("value of num_1 is : ",num_1)
+num_1 += 20
+print("value of num_1 is : ",num_1)
+num_1 -= 20
+print("value of num_1 is : ",num_1)
+num_1 *= 20 
+print("value of num_1 is : ",num_1)
+num_1 /= 20 
+print("value of num_1 is : ",num_1)
+num_1 %= 20
+print("value of num_1 is : ",num_1)
+num_1 //= 20 
+print("value of num_1 is : ",num_1)
+num_ = 123
+num_1 **= 20 
+print("value of num_1 is : ",num_1)
