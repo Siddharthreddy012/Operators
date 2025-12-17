71d7c1ba1eb9dea39cd573ee3bc32524a9c87130
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-12-17 19:26:12 +0530

    operators

---
+++ Comparison/Com.py
@@ -0,0 +1,25 @@
+#we 
+person_1 =54
+person_2 =80
+com = person_1<person_2
+print(com)
+
+#he 
+person_3 =5.8
+person_4 =6.0
+com= person_4>person_3
+print(com)
+
+#we,he
+person_5=(75,5.5)
+person_6=(50,5.5)
+com = person_5[0] > person_6[0]
+print(com)
+
+com= person_5[1] == person_6[1]
+print(com)
+
+# age 
+person_7 = 18
+com=person_7<=18
+print(com)
